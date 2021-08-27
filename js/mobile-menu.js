6f988280de6a32920a6063eb202e9aa820192b5b
Kateryna Kolomiiets <kateryna.kolomiiets.official@gmail.com>
2021-08-27 11:59:29 +0300

    submit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,12 +1,12 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector('.menu-toggler'),
-        // closeMenuBtn: document.querySelector('.menu-close'),
+        openMenuBtn: document.querySelector('.menu-opened'),
+        closeMenuBtn: document.querySelector('.menu-closed'),
         menu: document.querySelector('.mobile-menu'),
     };
 
     refs.openMenuBtn.addEventListener('click', toggleMenu);
-    // refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
     function toggleMenu() {
         refs.menu.classList.toggle('is-hidden');
